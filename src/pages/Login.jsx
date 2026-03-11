110e0314148249e466be8840d2b5975e3de172d9
L10TOYA <112649759+L10TOYA@users.noreply.github.com>
2026-03-11 15:36:17 -0400

    fixed logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,21 +3,21 @@ import logoImage from "../assets/husyrentlens.png";
 import googleImage from "../assets/google.png";
 import { Link } from 'react-router-dom';
 
-
 const Login = () => {
   return (
     <>
-      <div className='grid grid-cols-2 h-screen   '>
-        <div className='flex flex-col justify-center items-center pb-30'>
           <Link to={"/"}>
-            <img src={logoImage} className="w-40 h-auto fixed top-0 left-10" alt="Logo" />
+            <img src={logoImage} className="w-40 h-auto fixed top-6 left-10 rounded-4xl" alt="Logo" />
           </Link>
-          <p className='text-6xl pb-10 font-extrabold'>search. compare. review.</p>
+      <div className='grid grid-cols-1 md:grid-cols-2 h-screen'>
+        <div className='hidden md:flex flex-col justify-center items-center pb-30'>
+          <p className='text-6xl pb-10 font-extrabold text-center'>search. compare. review.</p>
           <p className='text-xl'>Find Your Next Home Near Campus</p>
           <p className='text-xl'>Real reviews from students. No more surprise</p>
         </div>
+
         <div className='bg-yellow-400 h-full w-full flex justify-center items-center'>
-          <div className='bg-gray-200 w-[420px] p-10 rounded-2xl flex flex-col gap-6'>
+          <div className='bg-gray-200 w-[90%] md:w-[420px] p-10 rounded-2xl flex flex-col gap-6'>
             <div className='flex flex-col gap-2'>
               <label className='text-lg'>Email</label>
               <input type="text" className='h-12 rounded-md px-3 bg-gray-100 focus:outline-none'/>
