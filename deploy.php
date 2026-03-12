cf186c274dc163610dcf8c8e40821dbd6dfcea32
zph-mtek <zphorsch@mtu.edu>
2026-03-11 22:08:51 -0400

    Test 2

--- deploy.php
+++ deploy.php
@@ -1,5 +1,6 @@
 <?php
-// Get the JSON payload from GitHub.
+// Get the JSON payload from GitHub. 
+// Testing
 $content = file_get_contents("php://input");
 $payload = json_decode($content, true);
 
