e41043fc78a6be0df3f5838fcabd52c948e31f3e
L10TOYA <112649759+L10TOYA@users.noreply.github.com>
2026-04-03 14:26:10 -0400

    fixed some issue

--- backend/addProperty.php
+++ backend/addProperty.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
