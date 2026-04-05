156b07814da5231bb28aa4ccbac46dc320379020
L10TOYA <112649759+L10TOYA@users.noreply.github.com>
2026-04-05 15:57:12 -0400

    fixed some jwt issues

--- src/API/sendPropertyData.js
+++ src/API/sendPropertyData.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 export const submitPropertyData = async (formData) => {
   const API_BASE_URL = 'https://huskyrentlens.cs.mtu.edu';
-  const response = await axios.post(`${API_BASE_URL}/backend/addProperty.php`, formData);
+  
+  const token = localStorage.getItem('token');
+
+  const response = await axios.post(`${API_BASE_URL}/backend/addProperty.php`, formData, {
+    headers: {
+        'Content-Type': 'multipart/form-data', 
+        'Authorization': `Bearer ${token}`     
+    }
+  });
 
   return response.data;
 };
