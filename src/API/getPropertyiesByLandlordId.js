23fae7ebfebfedfb4d2c5a6e2a1d1e4da5bcc1b3
L10TOYA <112649759+L10TOYA@users.noreply.github.com>
2026-04-05 14:00:03 -0400

    added js file to get by landlord id

---
+++ src/API/getPropertyiesByLandlordId.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://huskyrentlens.cs.mtu.edu';
+
+export const getPropertyiesByLandlordId = async (id) => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/backend/fetchPropertyByLandlordId.php?id=${id}`);
+    
+        return response.data;
+    } catch (error) {
+        console.error("API connection error:", error);
+        throw error;
+    }
+};
+
